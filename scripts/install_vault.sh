3a733bd12e5bfc247b6b22ac689cb799ac5d3540
danfedick <daniel.fedick@hashicorp.com>
2022-01-26 15:40:51 -0500

    Moving old scripts

--- scripts/install_vault.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -ex
-
-echo "Adding Vault repo"
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
-echo "Install Vault binary with apt"
-sudo apt-get update
-sudo apt-get install -y vault
