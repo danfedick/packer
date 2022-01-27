709ab235b92a32eb703c6660a148ac83f8c0f98a
danfedick <daniel.fedick@hashicorp.com>
2022-01-27 06:07:33 -0500

    Adding consul binary into packer image

--- aws/scripts/packer_install.sh
+++ aws/scripts/packer_install.sh
@@ -1,15 +1,9 @@
-#!/bin/sh -ex
-#
-#
-#
-#
+#!/bin/sh
+#  Name: system_setup.sh
+#  Author: Dan Fedick
 #######################################
+# set -x # Uncomment to Debug
       
-echo "Installing System Packages"
-sudo apt-get update && sudo apt-get -y install software-properties-common
-
-
-
 echo "Installing Docker-Compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
@@ -22,3 +16,9 @@ echo "Install Vault binary with apt"
 sudo apt-get update && sudo apt-get install vault -y 
 
 echo "Completed Vault Installation"
+
+echo "Install Consul Binary"
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install consul
+
