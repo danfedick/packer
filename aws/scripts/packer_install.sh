a24f40d481c4c698bb4bb8e4850a31b9c0344f42
danfedick <daniel.fedick@hashicorp.com>
2022-01-28 11:15:06 -0500

    moving apt-get update after the repository add

--- aws/scripts/packer_install.sh
+++ aws/scripts/packer_install.sh
@@ -10,8 +10,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Adding Vault repo"
 sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
-sudo apt-get update && sudo apt-get install vault 
-sudo apt-get update && sudo apt-get install consul
-sudo apt-get update && sudo apt-get install nomad 
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && sudo apt-get update
+sudo apt-get install vault 
+sudo apt-get install consul
+sudo apt-get install nomad 
